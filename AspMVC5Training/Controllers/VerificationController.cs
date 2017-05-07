ec250a9543a12e9571084442bab3e9bd12c9f610
sam <evaoosyz@gmail.com>
2017-05-07 22:36:07 +0800

    加入 Remote 驗證

---
+++ AspMVC5Training/Controllers/VerificationController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspMVC5Training.Controllers
+{
+    public class VerificationController : Controller
+    {
+        // GET: Verification
+        public ActionResult IpV6(string ipV6)
+        {
+            IPAddress inputValue;
+            if (IPAddress.TryParse(ipV6?.ToString() ?? string.Empty, out inputValue) &&
+                inputValue.AddressFamily == AddressFamily.InterNetworkV6)
+            {
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(false, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
--- AspMVC5Training/ViewModel/DataViewModel.cs
+++ AspMVC5Training/ViewModel/DataViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using AspMVC5Training.Infrastructure.CustomAttribute;
 using System.ComponentModel.DataAnnotations;
 using System.Net;
+using System.Web.Mvc;
 
 namespace AspMVC5Training.ViewModel
 {
@@ -16,6 +17,9 @@ namespace AspMVC5Training.ViewModel
         [IpV4(ErrorMessage ="注意寫這裡沒用")]
         public string IpV4 { get; set; }
 
+        //[Remote("IpV6", "Verification", ErrorMessage = "IpV6有問題喔 請在檢查看看")]
+        [Remote("IpV6", "Valid", "Tools", ErrorMessage = "IpV6有問題喔 請在檢查看看")]
+        
         public string IpV6 { get; set; }
 
         [EmailAddress]
